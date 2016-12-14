2445eba85eb87797ce6d9b31d9e8a84f9e4e6f72
Hanool Kim <excelsior0828@gmail.com>
2016-12-14 20:04:06 +0900

    edit web.js

--- web.js
+++ web.js
@@ -10,6 +10,6 @@ var byl = require('./routes/byl')();
 app.use('/byl', byl);
 
 //connection
-app.listen(3000, function(){
-  console.log('Connected 3000 port!')
+app.listen(5000, function(){
+  console.log('Connected 5000 port!')
 });
